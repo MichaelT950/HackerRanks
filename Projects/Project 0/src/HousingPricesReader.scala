b91c6104b51d23979b740aefcc0846f0e2cee7a7
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-12 21:00:16 -0600

    ADDED FUNCTIONS: - initState() - printHouses()

--- Projects/Project 0/src/HousingPricesReader.scala
+++ Projects/Project 0/src/HousingPricesReader.scala
@@ -12,17 +12,29 @@ object HousingPricesReader extends App{
   //INITIATE APP
   println("Here are a list of commands: " )
   println(readInst())
+  initState()
 
   //POST FUNCTIONS HERE
   //make a read instructions function
   def readInst () ={
     for (ln <- helpFile.getLines()) println(ln)
   }
+  def initState() = {
+    println("What would you like to do?")
+    println("  1 - Print Values    ")
 
-  val file = io.Source.fromFile(fileName)
-  for (ln <- file.getLines().drop(1)){
-    val Array( index,sqft, beds, baths, zip, year, price) = ln.split(",").map(_.trim)
-    //println( s"$index $sqft $beds $baths $zip $year $price")
+    val cmd = scala.io.StdIn.readLine()
+    val init =  cmd match {
+      case "1" => printHouses()
+    }
+  }
+
+  def printHouses() = {
+    val file = io.Source.fromFile(fileName)
+    for (ln <- file.getLines().drop(1)) {
+      val Array(index, sqft, beds, baths, zip, year, price) = ln.split(",").map(_.trim)
+      println(s"$index $sqft $beds $baths $zip $year $price")
+    }
   }
 
 }
