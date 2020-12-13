87ce0bdac7890726a24baeed838ae02a4914b043
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-12 20:25:51 -0600

    ADDED FUNCTIONALITY

--- Projects/Project 0/src/HousingPricesReader.scala
+++ Projects/Project 0/src/HousingPricesReader.scala
@@ -1,12 +1,28 @@
-/**
- * Reads Zillow housing prices csv file and stores rows in an array
+/**HousingPricesReader
+ *
  */
 object HousingPricesReader extends App{
 
-  val file = io.Source.fromFile("zillow.csv")
+  println("What file would you like to read?")
+
+  val fileName = scala.io.StdIn.readLine()
+  val helpInstructions = "HelpInstructions"
+  val helpFile = io.Source.fromFile(helpInstructions)
+
+  //INITIATE APP
+  println("Here are a list of commands: " )
+  println(readInst())
+
+  //POST FUNCTIONS HERE
+  //make a read instructions function
+  def readInst () ={
+    for (ln <- helpFile.getLines()) println(ln)
+  }
+
+  val file = io.Source.fromFile(fileName)
   for (ln <- file.getLines().drop(1)){
     val Array( index,sqft, beds, baths, zip, year, price) = ln.split(",").map(_.trim)
-    println(s"$index $sqft $beds $baths $zip $year $price")
+    //println( s"$index $sqft $beds $baths $zip $year $price")
   }
 
 }
